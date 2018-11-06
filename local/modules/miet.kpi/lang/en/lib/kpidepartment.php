45a072336a32b621c20ab122a19448d23f66c883
Maxim Bayurov <vakakak20@gmail.com>
2018-11-06 11:20:50 +0300

    DAO KPI department has been created

--- local/modules/miet.kpi/lang/en/lib/kpidepartment.php
+++ local/modules/miet.kpi/lang/en/lib/kpidepartment.php
@@ -5,3 +5,10 @@
  * Date: 06.11.2018
  * Time: 10:28
  */
+?>
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "KPI value";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Entering date";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Last change date";
+?>
--- local/modules/miet.kpi/lang/ru/lib/kpidepartment.php
+++ local/modules/miet.kpi/lang/ru/lib/kpidepartment.php
@@ -5,3 +5,12 @@
  * Date: 06.11.2018
  * Time: 10:28
  */
+?>
+<?
+$MESS["KPI_ENTITY_ID_FIELD"] = "ID KPI";
+$MESS["KPI_ENTITY_UF_VALUE_FIELD"] = "Значение показателя";
+$MESS["KPI_ENTITY_UF_CREATED_FIELD"] = "Дата внесения значения
+показателя";
+$MESS["KPI_ENTITY_UF_CHANGED_FIELD"] = "Дата последнего изменения
+значения показателя";
+?>
